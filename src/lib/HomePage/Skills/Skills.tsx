032b39ab28919d582191c4b7719cf1340e5fe488
sakib <mohammadsakib7679@gmail.com>
2024-07-28 01:09:31 +0600

    created skill section

--- src/lib/HomePage/Skills/Skills.tsx
+++ src/lib/HomePage/Skills/Skills.tsx
@@ -1,8 +1,16 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 
-const Skills = () => {
+const Skills = async () => {
+  const res = await fetch("http://localhost:5000/api/v1/skill", {
+    next: {
+      revalidate: 30,
+    },
+  });
+  const { data: skills } = await res.json();
+
   return (
-    <Container>
+    <Container sx={{ pb: 8 }}>
       <Box textAlign="center">
         <Typography
           variant="h3"
@@ -17,6 +25,32 @@ const Skills = () => {
           project that inspires you and you customers.
         </Typography>
       </Box>
+      <Stack
+        direction="row"
+        gap={6}
+        py={5}
+        justifyContent="center"
+        alignItems="center"
+      >
+        {skills?.map((skill: any) => (
+          <Box key={skill.id}>
+            <Box sx={{ backgroundColor: "lightgray", boxShadow: 1, p: 2 }}>
+              <Image
+                height={100}
+                width={100}
+                src={skill?.image}
+                alt="skill image"
+              />
+              <Typography textAlign="center" pt={2} fontWeight={600}>
+                {skill?.parcentage}%
+              </Typography>
+            </Box>
+            <Typography textAlign="center" fontWeight={600} pt={2}>
+              {skill.name}
+            </Typography>
+          </Box>
+        ))}
+      </Stack>
     </Container>
   );
 };
