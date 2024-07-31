a64125e656e362a29bd2afd4f2b0bc57976403ed
sakib <mohammadsakib7679@gmail.com>
2024-07-31 18:20:38 +0600

    added data grid table for showing data

--- src/app/(withDashboardLayout)/dashboard/admin/skill/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/skill/page.tsx
@@ -3,11 +3,19 @@ import { Box, Button, Stack, TextField } from "@mui/material";
 import SkillModal from "./components/SkillModal";
 import { useState } from "react";
 import { useGetAllSkillQuery } from "@/redux/api/skillApi";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Skill = () => {
   const { data, isLoading } = useGetAllSkillQuery({});
   console.log(data);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const columns: GridColDef[] = [
+    { field: "image", headerName: "Image", width: 70 },
+    { field: "name", headerName: "Skill", width: 70 },
+    { field: "parcentage", headerName: "Parcentage", width: 70 },
+  ];
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between">
@@ -16,8 +24,22 @@ const Skill = () => {
         <TextField placeholder="Search Skill" />
       </Stack>
 
-      <Box>
-        <h2>display data</h2>
+      <Box mt={5}>
+        {isLoading ? (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          <Box style={{ width: "100%" }}>
+            <DataGrid rows={data} columns={columns} hideFooter />
+          </Box>
+        )}
       </Box>
     </Box>
   );
