11f5ecc2d543f5a2779dbf2ab54197d0e3b11af3
sakib <mohammadsakib7679@gmail.com>
2024-07-31 12:38:29 +0600

    handle request and response in axios instance

--- src/app/(withDashboardLayout)/dashboard/admin/skill/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/skill/page.tsx
@@ -1,8 +1,15 @@
+import Modal from "@/components/Shared/Modal/Modal";
+import { Box, Button, Stack, TextField } from "@mui/material";
+
 const Skill = () => {
   return (
-    <div>
-      <h2>This is Skill component</h2>
-    </div>
+    <Box>
+      <Stack direction="row" justifyContent="space-between">
+        <Button>Add New Skill</Button>
+        <Modal />
+        <TextField placeholder="Search Skill" />
+      </Stack>
+    </Box>
   );
 };
 
