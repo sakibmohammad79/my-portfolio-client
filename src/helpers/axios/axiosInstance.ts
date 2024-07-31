8c01680423a9f92955bb8f3534da8cc48cbb81d0
sakib <mohammadsakib7679@gmail.com>
2024-07-31 12:22:52 +0600

    handle request and response in axios instance

--- src/helpers/axios/axiosInstance.ts
+++ src/helpers/axios/axiosInstance.ts
@@ -25,14 +25,28 @@ instance.interceptors.request.use(
 
 // Add a response interceptor
 instance.interceptors.response.use(
+  //@ts-ignore
   function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    return response;
+    const responseObject: { data: any } = {
+      data: response?.data?.data,
+    };
+    return responseObject;
   },
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    return Promise.reject(error);
+    const responseObject: {
+      statusCode: number;
+      message: string;
+      errorMessages: any;
+    } = {
+      statusCode: error?.response?.data?.statusCode || 500,
+      message: error?.response?.data?.message || "Something went wrong!",
+      errorMessages: error?.response?.data?.message,
+    };
+    //return Promise.reject(error);
+    return responseObject;
   }
 );
