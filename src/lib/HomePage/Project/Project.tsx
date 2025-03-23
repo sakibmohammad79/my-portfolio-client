350f7ac3c5e65e432476ff290e358a24c4fda0f3
sakib <mohammadsakib7679@gmail.com>
2025-03-23 12:26:20 +0600

    improve project section

--- src/lib/HomePage/Project/Project.tsx
+++ src/lib/HomePage/Project/Project.tsx
@@ -10,14 +10,9 @@ import {
 } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const Project = () => {
-  // const res = await fetch("http://localhost:5000/api/v1/project", {
-  //   next: {
-  //     revalidate: 10,
-  //   },
-  // });
-  // const { data: projects } = await res.json();
   const { data } = useGetAllProjectQuery({});
 
   return (
@@ -29,47 +24,64 @@ const Project = () => {
         <Typography
           pb={6}
           sx={{
-            fontSize: {
-              xs: 30,
-              md: 30,
-              sm: 30,
-              lg: 45,
-            },
+            fontSize: { xs: 30, md: 30, sm: 30, lg: 45 },
           }}
           fontWeight={600}
         >
           MY PROJECT
         </Typography>
       </Box>
-      <Grid container spacing={3} justifyContent="center" alignItems="center">
+      <Grid container spacing={3} justifyContent="center" alignItems="stretch">
         {data?.map((project: any) => (
-          <Grid item xs={12} sm={12} md={6} lg={4} xl={4} key={project.id}>
-            <Link href={`/project/${project.id}`}>
-              <Card sx={{ p: 3, height: 400, borderRadius: 4 }}>
-                <Image
-                  style={{ borderRadius: 4 }}
-                  src={project?.image}
-                  height={300}
-                  width={500}
-                  alt="project-image"
-                />
-                <CardContent>
-                  <Typography color="primary.main">WEBSITE</Typography>
-
-                  <Typography
-                    sx={{
-                      "&:hover": {
-                        color: "primary.main",
-                      },
-                      fontSize: { xs: 20, md: 20, sm: 20, lg: 30 },
-                    }}
-                    component="h1"
-                    fontWeight={520}
+          <Grid item xs={12} sm={12} md={6} lg={4} key={project.id}>
+            <Link
+              href={`/project/${project.id}`}
+              style={{ textDecoration: "none" }}
+            >
+              <motion.div
+                whileHover={{
+                  scale: 1.05,
+                  boxShadow: "0px 4px 20px rgba(0,0,0,0.2)",
+                }}
+                transition={{ duration: 0.3 }}
+              >
+                <Card
+                  sx={{
+                    p: 3,
+                    height: "100%",
+                    borderRadius: 4,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  <Box
+                    sx={{ width: "100%", height: 250, position: "relative" }}
                   >
-                    {project?.title}
-                  </Typography>
-                </CardContent>
-              </Card>
+                    <Image
+                      src={project?.image}
+                      alt="project-image"
+                      layout="fill"
+                      objectFit="cover"
+                      style={{ borderRadius: 4 }}
+                    />
+                  </Box>
+                  <CardContent>
+                    <Typography color="primary.main">WEBSITE</Typography>
+                    <Typography
+                      sx={{
+                        "&:hover": { color: "primary.main" },
+                        fontSize: { xs: 20, lg: 25 },
+                        fontWeight: 520,
+                      }}
+                      component="h1"
+                    >
+                      {project?.title}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </motion.div>
             </Link>
           </Grid>
         ))}
