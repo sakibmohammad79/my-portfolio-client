ec451b40bf1fabb1ec7b6450a47f9f0e36d149ff
sakib <mohammadsakib7679@gmail.com>
2025-09-06 17:51:22 +0600

    add my image instead of demo image in banner

--- src/lib/HomePage/Banner/Banner.tsx
+++ src/lib/HomePage/Banner/Banner.tsx
@@ -488,8 +488,8 @@ const Banner = () => {
                       }}
                       height={500}
                       width={600}
-                      src="https://i.postimg.cc/3xB8zkfr/coding.webp"
-                      alt="Backend Developer Coding"
+                      src="https://i.postimg.cc/Nf7NP0y9/Gemini-Generated-Image-3uuyfs3uuyfs3uuy.png"
+                      alt="Md. Sakib - Backend Developer"
                     />
                   </motion.div>
                 </Box>
